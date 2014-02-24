5d0f9b76c58a6d83fb4d7d4a89132bb507b2c367
isaackyuka <zergakyuk@gmail.com>
2014-02-24 10:40:38 +0300

    First release
    Banking guide project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Akatale</title>
+<link href="css/main.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="header"><h1>Akatale.com</h1></div>
+<div id="container">
+  <div id="main"></div>
+  <div id="sidebar"></div>
+</div>
+<div id="footer"></div>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,3 @@
+A Banking Guide
+
+Yo one stop site for any financial and banking decisions
